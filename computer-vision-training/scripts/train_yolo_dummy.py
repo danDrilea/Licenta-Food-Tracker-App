988d898641fae52d31ef7789c08da26152e431c3
danDrilea <danalexandrudrilea@gmail.com>
2026-03-11 12:06:46 +0200

    tested training for 1 epoch

---
+++ computer-vision-training/scripts/train_yolo_dummy.py
@@ -0,0 +1,31 @@
+from ultralytics import YOLO, settings
+import os
+
+# Automatically find the paths
+DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+settings.update({"datasets_dir": DIR})  # Set the datasets directory to the parent of the current script
+
+YAML_PATH = os.path.join(DIR, 'food.yaml')
+RUNS_DIR = os.path.join(DIR, 'runs')
+
+def main():
+    print("Loading YOLO11 Small Segmentation model...")
+    model = YOLO("yolo11s-seg.pt") 
+
+    print("Starting training on GPU")
+
+    results = model.train(
+        data=YAML_PATH,
+        epochs=1,      
+        imgsz=640,       
+        batch=8,         
+        device=0,        
+        workers=4,
+        project=RUNS_DIR,         # Saves output cleanly in computer-vision-training/runs
+        name="FoodInsSeg_DEMO_Run1" 
+    )
+    
+    print("✅ Training complete!")
+
+if __name__ == "__main__":
+    main()
