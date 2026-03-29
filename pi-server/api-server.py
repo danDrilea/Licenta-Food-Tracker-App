6eb4605fd05f5ea0fe2913eca9ee598619d1b642
danDrilea <drileadanalexandru@gmail.com>
2026-03-29 18:24:18 +0300

    implementare endpoint care primeste o poza si o trimite inapoi exact la fel LOL

--- pi-server/api-server.py
+++ pi-server/api-server.py
@@ -1,7 +1,29 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, File, UploadFile
+from fastapi.responses import JSONResponse
+import base64
 
-app = FastAPI()
+app = FastAPI(title="AI Food Tracker API")
 
 @app.get("/")
 async def root():
-    return {"message" : "Hello World"}
+    return {"message": "Raspberry Pi Server is Live!"}
+
+@app.post("/analyze-food")
+async def analyze_food(photo: UploadFile = File(...)):
+    try:
+        image_bytes = await photo.read()
+
+        encoded_image = base64.b64encode(image_bytes).decode("utf-8")
+
+        return JSONResponse(content={
+            "status": "success",
+            "message": "Image processed successfully",
+            "original_filename": photo.filename,
+            "image_base64": encoded_image 
+        })
+
+    except Exception as e:
+        return JSONResponse(
+            content={"status": "error", "message": str(e)}, 
+            status_code=500
+        )
